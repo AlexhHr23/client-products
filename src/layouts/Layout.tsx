4a4a7d11a8be105f1bed1d4d7c49a0cdf5c7da5a
ALexhHr23 <alex.hernandez24kb@gmail.com>
2025-05-05 21:32:23 -0600

    Trabajando con el layout

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -2,10 +2,19 @@ import { Outlet } from "react-router-dom"
 
 
 export const Layout = () => {
-  return (
-    <>
-        <div>Layout</div>
-        <Outlet/>
-    </>
-  )
+    return (
+        <>
+            <header className="bg-slate-800">
+                <div className="mx-auto max-w-6xl py-10">
+                    <h1 className="text-4xl font-extrabold text-white">
+                        Administrador de productos
+                    </h1>
+                </div>
+            </header>
+
+            <main className="mt-10 mx-auto max-w-6xl py-10 p-10 bg-white shadow">
+                <Outlet />
+            </main>
+        </>
+    )
 }
