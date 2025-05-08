e877f648ab6846422ff19a619f43d721794cfe5d
ALexhHr23 <alex.hernandez24kb@gmail.com>
2025-05-07 21:01:42 -0600

    Uso de useNavigate y LInk

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,3 +1,4 @@
+import {useNavigate } from "react-router-dom"
 import type { Product } from "../types"
 import { formatCurrency } from "../utils"
 
@@ -6,6 +7,8 @@ type ProductDetailsProps = {
 }
 
 export const ProductDetails = ({product}: ProductDetailsProps) => {
+
+    const navigate = useNavigate()
 
     const isAvailable = product.availability
 
@@ -21,7 +24,13 @@ export const ProductDetails = ({product}: ProductDetailsProps) => {
                 {isAvailable ? 'Disponible' : 'No disponible'}
             </td>
             <td className="p-3 text-lg text-gray-800 ">
-                
+                <div className="flex gap-2 items-center">
+                    <button
+                        onClick={() => navigate(`/products/${product.id}/edit`)}
+                        className="bg-indigo-600 text-white w-full p-2 font-bold uppercase text-xs text-center rounded-lg"
+                    >
+                        Editar</button>
+                </div>
             </td>
         </tr>
     )
